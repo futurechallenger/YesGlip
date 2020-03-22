4cb8422ce6c370b09853ecbb1bb0d27e47229919
Bruce Li <bruce.li@BrucedeMacBook-Pro.local>
2020-03-22 13:36:24 +0800

    no same origin

--- lib/src/controllers/home.dart
+++ lib/src/controllers/home.dart
@@ -29,10 +29,7 @@ class HomeController extends Controller {
 
       // res.redirect('/');
       res.headers['X-Frame-Options'] = 'ALLOW-FROM https://instructure.com';
-      if(res.headers.containsKey('X-Frame-Options')){
-        res.headers.remove('X-Frame-Options');
-        // res.headers['Content-Security-Policy'] = 'frame-ancestors https://*.instructure.com';
-      }
+      // res.headers['Content-Security-Policy'] = 'frame-ancestors https://*.instructure.com';
       await res.render('index', {'title': 'First render'});
     } catch (e) {
       print('ERROR: ${e}');
